10ca8c2dcac6f6c9a31e1cb4e53eea5e7f22b995
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-07-15 18:57:24 -0400

    commit assets
    css
    js

---
+++ ViajesChile/assets/js/scripts-jq.js
@@ -0,0 +1,12 @@
+
+//jq para ocultar
+$('.card-title').click(function(){
+    $('.destacados p').toggle("hide");
+})
+
+//jq para alert de que se envió un correo
+$('#enviar_correo').click(function () {
+
+    alert("El correo fue enviado correctamente...")
+})
+
