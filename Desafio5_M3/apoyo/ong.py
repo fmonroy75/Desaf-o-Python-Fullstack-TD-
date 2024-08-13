da5a2c74a80cd80bddbb462ba6d0e00a62e629c0
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-12 22:15:43 -0400

    update: implementación función calcular

--- Desafio5_M3/apoyo/ong.py
+++ Desafio5_M3/apoyo/ong.py
@@ -1,3 +1,7 @@
+#modo de ejecucion por terminal
+#python ong.py "fact_1 = 5; prod_1 = [3,6,4,2,8]; fact_2 = 6"
+
+
 import sys
 i=0
 
@@ -18,16 +22,61 @@ def producto(arr):
 
 
 
+# Función de control de operaciones
+def calcular(**kwargs):
+    resultados = {}
+    for clave, valor in kwargs.items():
+        if clave.startswith('fact_'):
+            resultados[clave] = factorial(valor)
+            print(f"El factorial de {resultados[clave]} es {factorial(valor)}")
+        elif clave.startswith('prod_'):
+            resultados[clave] = producto(valor)
+            print(f"La productoria de {resultados[clave]} es {producto(valor)}")
+        else:
+            raise ValueError(f"Operación no reconocida: {clave}")
+    return 
 
-for operatorias in sys.argv:
-    if i>0:  
-        if "fact" in operatorias:
-            arr_calculo=operatorias.split("=")
-            print(f"El factorial de {arr_calculo[1]} es {factorial(int(arr_calculo[1]))}")
-        if "prod" in operatorias:
-            arr_calculo=operatorias.split("=")
-            print(f"La productoria de {arr_calculo[1]} es {producto(eval(arr_calculo[1]))}")
-    i=i+1
+
+if len(sys.argv)>1:
+    sys.argv.pop(0)
+
+    print(sys.argv[0])
+
+
+
+     # Procesar argumentos manualmente
+    argumentos = {}
+    arr_aux=sys.argv[0].split(';')
+    for arg in arr_aux:
+        clave, valor = arg.split('=')
+        clave = clave.strip()
+        #print(valor)
+        # Determinar si el valor es un entero o una lista
+        if "[" in valor and "]" in valor:
+            valor=eval(valor)
+        else:
+            valor = int(valor.strip())
+        argumentos[clave] = valor
+    print("por argumentos")
+
+    #print(sys.argv[0])
+    #print(argumentos)
+    calcular(**argumentos)
+    
+else:
+
+    print("ejecucion desde script")
+    calcular(fact_1 = 5, prod_1 = [3,6,4,2,8], fact_2 = 6)
+
+#for operatorias in sys.argv:
+#    if i>0:  
+#        if "fact" in operatorias:
+#            arr_calculo=operatorias.split("=")
+#            print(f"El factorial de {arr_calculo[1]} es {factorial(int(arr_calculo[1]))}")
+#        if "prod" in operatorias:
+#            arr_calculo=operatorias.split("=")
+#            print(f"La productoria de {arr_calculo[1]} es {producto(eval(arr_calculo[1]))}")
+#    i=i+1
 
 
 #calcular(fact_1 = 5, prod_1 = [3,6,4,2,8], fact_2 = 6)
